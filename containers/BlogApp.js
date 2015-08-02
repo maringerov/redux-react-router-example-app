85b0374a18c34820a4d2b07ef90f37a8870047b1
Marin Gerov <marin.gerov@gmail.com>
2015-08-02 20:17:35 +0200

    Extrated select method as a separate function from Connector

--- containers/BlogApp.js
+++ containers/BlogApp.js
@@ -4,13 +4,14 @@ import { Connector } from 'react-redux';
 import Main from '../components/Main';
 import * as BlogActions from '../actions/BlogActions';
 
+function select(state) {
+  return { blogposts: state.blogposts}
+}
 
 export default class BlogApp extends Component {
   render() {
     return (
-      <Connector select={state => ({
-          blogposts: state.blogposts
-        })}>
+      <Connector select={select}>
         {this.renderChild}
       </Connector>
     );
